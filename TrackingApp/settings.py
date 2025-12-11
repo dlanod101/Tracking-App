e3bada1854842f6a9e4edabdc7ab3d40cf865feb
Donald Nsikhe <dnsikhe@gmail.com>
2025-12-11 20:32:55 +0100

    ConnectedDB

--- TrackingApp/settings.py
+++ TrackingApp/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'TrackingApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_BmGht8v6fegI',
+        'HOST': 'ep-fancy-bonus-ads59ida-pooler.c-2.us-east-1.aws.neon.tech',
+        'PORT': '',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
